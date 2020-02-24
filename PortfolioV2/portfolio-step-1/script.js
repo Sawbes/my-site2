f0a9f85b99fb0cc1f14cbd19e63da5fbb1593d32
Sawbes <60784898+Sawbes@users.noreply.github.com>
2020-02-24 19:43:31 +0300

    починка кнопки

--- PortfolioV2/portfolio-step-1/script.js
+++ PortfolioV2/portfolio-step-1/script.js
@@ -4,13 +4,13 @@
       link = document.createElement('link');
   link.rel = 'stylesheet';
   // проверяем значение из localStorage если dark то темная тема
-  if (localStorage.getItem('themeStyle') === 'dark') {
-    link.href = 'dark.css'; // ссылка на темный стиль
+  if (localStorage.getItem('themeStyle') === 'light') {
+    link.href = 'light.css'; // ссылка на темный стиль
     document.getElementById('switch-1').setAttribute('checked', true); // переключаем чекбокс в положение "темная тема"
   }
   // по умолчанию светлая тема
   else {
-    link.href = 'dark.css'; // ссылка на светлый стиль
+    link.href = 'light.css'; // ссылка на светлый стиль
   }
   head.appendChild(link); // вставляем <link rel="stylesheet" href="light|dark.css"> в шапку страницы между темаги head
 
@@ -19,11 +19,11 @@
     let btn = ev.target;
     // если чекбокс включен
     if (btn.checked) {
-      link.href = 'dark.css'; // сключаем темную тему
-      localStorage.setItem('themeStyle', 'dark'); // записываем значение в localStorage
+      link.href = 'light.css'; // сключаем темную тему
+      localStorage.setItem('themeStyle', 'light'); // записываем значение в localStorage
     }
     else {
-      link.href = 'light.css'; // включаем светлую тему
-      localStorage.setItem('themeStyle', 'light'); // записываем значение в localStorage
+      link.href = 'dark.css'; // включаем светлую тему
+      localStorage.setItem('themeStyle', 'dark'); // записываем значение в localStorage
     }
   });
